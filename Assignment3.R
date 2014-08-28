faa6c076a33c334972ac8069679eb1391302a0d8
Jason Green <jason@convalytics.com>
2014-08-28 16:39:35 -0400

    Rprogramming Assmt3 Initial Code
    First bit of code

---
+++ Assignment3.R
@@ -0,0 +1,14 @@
+#  R Programming
+#  Assignment 3
+#  Jason Green
+#  8/28/2014
+
+#  This assignment will be graded using unit tests executed via the submit script that you run on your computer. To obtain the submit script, run the following code in R:
+   source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript3.R")
+   
+#   Or you can download the script to your working directory (NOTE: You may need to rename the script to be "submitscript3.R"). Then source it locally via
+#   source("submitscript3.R")
+   
+#   The first time you run the submit script it will prompt you for your Submission login and Submission password. 
+#   These can be found at the top of the Programming Assignments page. To execute the submit script, type
+   submit()
